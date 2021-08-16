02d72ba555d164b176e210404bcb2da0ae6b7571
ZareenZia801 <85026455+ZareenZia801@users.noreply.github.com>
2021-08-16 17:19:02 +0600

    Add files via upload

---
+++ admin/login/account/account.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="account.css" media="screen"/>
+    <title>Account</title>
+</head>
+
+<body>
+    
+
+
+             <div class="header">
+                <p class="home" >
+            
+                
+                
+                
+                
+                <span class="name">  <font color="#32174d" size="8"> Tuition/Tutor Care</font></p> </span>
+                
+</div>
+
+<section class="contSec">
+
+<div  class="contBox">
+
+           <center><h1>Account Database</h1></center>
+
+
+      </div>
+
+  </section>
+ 
+
+
+   
+
+   
+    
+
+<section class="footerSec">
+<div class="footer">
+<br/>
+<p align=center><b>Copyright <span>&#169;</span>2021</footer></b></p>
+</div>
+</section>
+
+          
